52451dffc66639d8b6074b41a445cef8b2959bfb
atareaoElvitaKehler <mekdy.20@gmail.com>
2021-10-22 20:43:19 -0300

    se avanza sobre logica de colision

--- TP3/EntregaFinal/js/main.js
+++ TP3/EntregaFinal/js/main.js
@@ -19,3 +19,37 @@ function cambiarClase(saltar){
 }
 sonic.addEventListener("animationend", function() { cambiarClase(false)})
 
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+let obstaculo = document.querySelector(".obstaculo");
+//let anillo = document.querySelector(".anillo");
+setInterval(detectarColision,1000);
+function detectarColision(){      
+    let sonicPos = sonic.getBoundingClientRect();       
+    let obstaculoPos =  obstaculo.getBoundingClientRect();
+    //let anilloPos =anillo.getBoundingClientRect();
+    let sonicWidht = sonicPos.left + sonicPos.width;
+    let sonicHeight = sonicPos.top + sonicPos.height;
+   // let anilloWidht = anilloPos.left + anilloPos.width;
+    //let anilloHeight = anilloPos.top + anilloPos.height;
+    let obstaculoWidht = obstaculoPos.left+obstaculoPos.width;
+    let obstaculoHeight = obstaculoPos.top+obstaculoPos.height;
+    if(sonicPos.left<=obstaculoWidht && sonicWidht>=obstaculoPos.left && sonicHeight>=obstaculoPos.top && sonicPos.top <= obstaculoHeight  ){
+  //  if(sonicWidht>=obstaculoPos.left && sonicPos.bottom<=obstaculoHeight){     
+        console.log("de frente");
+        console.log(sonicWidht>=obstaculoPos.left);
+        console.log("arriba");
+        console.log(sonicPos.bottom<=obstaculoHeight);
+
+     //   console.log(sonicPos.bottom<=obstaculoPos.top);
+    //    console.log("choque al saltar antes de pasar: "+sonicHeight>=obstaculoPos.top);
+     //   console.log("choque otro: "+sonicPos.top <= obstaculoHeight);
+        alert("murio")// restar vidas;
+  
+    }
+   // if(sonicPos.left<=anilloWidht && sonicWidht>=anilloPos.left && sonicHeight>=anilloPos.top && sonicPos.top <= anilloHeight )
+
+  //  alert("suma puntos")
+            
+  
+  }
