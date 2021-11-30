5f2a104b4c99f69f4d4e59e5b8ac744711c26f09
sergioyanez <sergiomyanez02@gmail.com>
2021-11-30 02:42:53 -0300

    Inicio de ringLoading

--- TP5/EntregaFinal/js/editarPerfil.js
+++ TP5/EntregaFinal/js/editarPerfil.js
@@ -3,12 +3,20 @@
 
 let btnVolver = document.getElementById("volver");
 btnVolver.addEventListener("click",volverAPerfil);
+let loader=document.getElementById("loader");
+
 
 let btnEditar = document.getElementById("botonEditar");
-btnEditar.addEventListener("click",volverAPerfil);
+btnEditar.addEventListener("click",realizarLoading);
 
 function volverAPerfil(){
     document.location.href ="perfil.html";
+}
+
+function realizarLoading(){
+loader.classList.remove("oculto");
+loader.classList.add("loading");
+setTimeout(volverAPerfil,1500);
 }
 
 
