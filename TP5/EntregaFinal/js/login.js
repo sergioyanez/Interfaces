6cb43a3f92e01e91153d6d1f645c7c7c8ebe0b89
sergioyanez <sergiomyanez02@gmail.com>
2021-12-02 03:58:19 -0300

    Se agrega hover en los botones y parallax en login.html

--- TP5/EntregaFinal/js/login.js
+++ TP5/EntregaFinal/js/login.js
@@ -16,3 +16,27 @@ function crearCuenta(){
     document.location.href ="registro.html";
 }
 
+const svg = document.getElementById('logoCompleto'),
+      logos = document.getElementById('.imagen1'),
+      image = document.getElementById('imagen2');
+
+function move(x, y) {
+  let dx = (x - window.innerWidth) / window.innerWidth * 10,
+      dy = (y - window.innerHeight) / window.innerHeight * 10;
+  svg.style.left = `${dx}vmin`;
+  svg.style.top = `${dy}vmin`;
+  dx += 5;
+  dy += 5 + 10;
+  logos.forEach((logo) => {
+    logo.setAttribute('transform', `translate(${dx} ${dy})`);
+  });
+  image.setAttribute('x', 0);
+}
+
+document.addEventListener('mousemove', (e) => {
+  move(e.pageX, e.pageY);
+});
+document.addEventListener('touchmove', (e) => {
+  move(e.touches[0].pageX, e.touches[0].pageY);
+});
+
